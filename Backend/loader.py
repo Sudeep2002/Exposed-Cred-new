9d99a2b61964129088d7b6d89a9550301030aee2
Sudeep2002 <sudeeppadaganur@gmail.com>
2026-03-12 21:47:20 +0530

    Load error so fixed

--- Backend/loader.py
+++ Backend/loader.py
@@ -4,7 +4,10 @@ def load_current_batch(path: str) -> pd.DataFrame:
     df = pd.read_excel(path)
     df["email"] = df["Email"].str.lower().str.strip()
     df["exposure_date"] = pd.to_datetime(df["Date of Exposure"])
-    df["source"] = df["Source"]
+    
+    # Safely handle 'Source' or 'source'
+    df["source"] = df["Source"] if "Source" in df.columns else df["source"]
+    
     return df[["email", "exposure_date", "source"]]
 
 
@@ -12,5 +15,8 @@ def load_master_data(path: str) -> pd.DataFrame:
     df = pd.read_excel(path)
     df["email"] = df["Email"].str.lower().str.strip()
     df["last_exposed_date"] = pd.to_datetime(df["Date of Exposure"])
-    df["source"] = df["Source"]  # <--- THIS WAS MISSING
+    
+    # Safely handle 'Source' or 'source'
+    df["source"] = df["Source"] if "Source" in df.columns else df["source"]
+    
     return df[["email", "last_exposed_date", "source"]]
